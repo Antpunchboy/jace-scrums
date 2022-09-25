e799e2f183bf250df3a565926d3966f4cbcc0361
Antpunchboy <jaceantman@yahoo.com>
2022-09-26 01:05:08 +0800

    fix save buttons (separate saving) (I understand how getItem and setItem works now)

--- javascript.js
+++ javascript.js
@@ -17,14 +17,42 @@ function joining() {
 
 // Load content onload if it exists in localStorage
 window.onload = function() {
-	if(localStorage.getItem('content')) {
-		document.querySelector('.content').innerHTML = localStorage.getItem('content');
-  }
+	if(localStorage.getItem('content1')) {
+        document.querySelector('.content1').innerHTML = localStorage.getItem('content1');
+  };
+    if(localStorage.getItem('content2')) {
+        document.querySelector('.content2').innerHTML = localStorage.getItem('content2');
+  };
+    if(localStorage.getItem('content3')) {
+        document.querySelector('.content3').innerHTML = localStorage.getItem('content3');
+  };
+    if(localStorage.getItem('content4')) {
+        document.querySelector('.content4').innerHTML = localStorage.getItem('content4');
+  };
+    if(localStorage.getItem('content5')) {
+        document.querySelector('.content5').innerHTML = localStorage.getItem('content5');
+  };
 }
 
-let editBtn = document.querySelector('#edit_content');
-let content = document.querySelector('.content');
+//let editBtn = document.querySelector('#edit_content');
+//let content = document.querySelector('.content');
 
-function saving() {
-    localStorage.setItem('content', content.innerHTML);
+function saving1() {
+    localStorage.setItem('content1', document.querySelector('.content1').innerHTML);
 };
+
+function saving2() {
+    localStorage.setItem('content2', document.querySelector('.content2').innerHTML);
+}
+
+function saving3() {
+    localStorage.setItem('content3', document.querySelector('.content3').innerHTML);
+}
+
+function saving4() {
+    localStorage.setItem('content4', document.querySelector('.content4').innerHTML);
+}
+
+function saving5() {
+    localStorage.setItem('content5', document.querySelector('.content5').innerHTML);
+}
